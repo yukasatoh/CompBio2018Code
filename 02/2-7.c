926e5122d34f726c45c3cb1f50581a81463e1cd0
yukasatoh <a.milkman0321@gmail.com>
2018-04-16 17:11:56 +0900

    update

---
+++ 02/2-7.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void){
+  double x0, xx, r, x, k;
+  int t;
+  x0=1.0;
+  r=3.0;
+  k=100.0;
+
+  FILE *fp;
+  fp=fopen("output.dat", "w");
+  fprintf(fp, "%d, %f\n", 0, x0);
+
+x=x0;
+  for(t=1; t<300; t++){
+    xx=x+r*(1-x/k)*x;
+    fprintf(fp, "%d, %f\n", t, xx);
+    x=xx;
+  }
+
+  fclose(fp);
+
+  return 0;
+}
