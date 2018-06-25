9023cfda26e2a4b048527cfb086b2f0b5f518cd1
yukasatoh <a.milkman0321@gmail.com>
2018-06-25 16:29:36 +0900

    update

--- 11/11-2.c
+++ 11/11-2.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include<stdio.h>
 
 int main(){
   int i,j;
@@ -6,16 +6,17 @@ int main(){
   double dt=0.01;
   double u[100][100];
   double utemp[100][100];
+  double v=1.0;
 
   double D=0.4;
 
-  FILE *fp;
+  FILE*fp;
 
-  fp=fopen("11-1.csv","w");
+  fp=fopen("11-2.csv","w");
 
-  //初期値
-  for(i=0; i<100; i++){
-    for(j=0; j<100; j++){
+  //初期化
+  for(i=0;i<100;i++){
+    for(j=0;j<100;j++){
       u[i][j]=0;
     }
   }
@@ -26,89 +27,83 @@ int main(){
   u[50][50]=1;
 
   //初期値出力
-  for(i=0; i<100; i++){
-    for(j=0; j<100; j++){
-      fprintf(fp, "%f", u[i][j]);
+  for(i=0;i<100;i++){
+    for(j=0;j<100;j++){
+      fprintf(fp,"%f",u[i][j]);
       if(j!=99){
-        fprintf(fp, " ,");
+        fprintf(fp,",");
       }
     }
-    fprintf(fp, "\n");
+    fprintf(fp,"\n");
   }
 
-  for(t=1; t<5000; t++){
+  for(t=1;t<5000;t++){
 
     //境界条件
-     //i=0, j=0
-     i=0;
-     j=0;
-     utemp[i][j]=u[i][j]+dt*(D*(u[99][j]+u[i+1][j]+u[i][99]+u[i][j+1]-4*u[i][j]));
+    //i=0,j=0
+    i=0;
+    j=0;
+    utemp[i][j]=u[i][j]+dt*(D*(u[99][j]+u[i+1][j]+u[i][99]+u[i][j+1]-4*u[i][j])+u[i][j]*u[i][j]/v-u[i][j]);
 
-     //i=0, j=99
-     i=0;
-     j=99;
-     utemp[i][j]=u[i][j]+dt*(D*(u[99][j]+u[i+1][j]+u[i][j-1]+u[i][0]-4*u[i][j]));
+    //i=0,j=99
+    i=0;
+    j=99;
+    utemp[i][j]=u[i][j]+dt*(D*(u[99][j]+u[i+1][j]+u[i][j-1]+u[i][0]-4*u[i][j])+u[i][j]*u[i][j]/v-u[i][j]);
 
-     //i=99, j=0
-     i=99;
-     j=0;
-     utemp[i][j]=u[i][j]+dt*(D*(u[i-1][j]+u[0][j]+u[i][99]+u[i][j+1]-4*u[i][j]));
+  //i=99,j=0
+  i=99;
+  j=0;
+  utemp[i][j]=u[i][j]+dt*(D*(u[i-1][j]+u[0][j]+u[i][99]+u[i][j+1]-4*u[i][j])+u[i][j]*u[i][j]/v-u[i][j]);
+  //i=99,j=99
+  i=99;
+  j=99;
+  utemp[i][j]=u[i][j]+dt*(D*(u[i-1][j]+u[0][j]+u[i][j-1]+u[i][0]-4*u[i][j])+u[i][j]*u[i][j]/v-u[i][j]);
 
-     //i=99, j=99
-     i=99;
-     j=99;
-     utemp[i][j]=u[i][j]+dt*(D*(u[i-1][j]+u[0][j]+u[i][j-1]+u[i][0]-4*u[i][j]));
-
-     //i=0
-     i=0;
-     for(j=1; j<99; j++){
-       utemp[i][j]=u[i][j]+dt*(D*(u[99][j]+u[i+1][j]+u[i][j-1]+u[i][j+1]-4*u[i][j]));
-     }
-
-     //i=99
-     i=99;
-     for(j=1; j<99; j++){
-       utemp[i][j]=u[i][j]+dt*(D*(u[i-1][j]+u[0][j]+u[i][j-1]+u[i][j+1]-4*u[i][j]));
-     }
-
-     //j=0
-     j=0;
-     for(i=1; i<99; i++){
-       utemp[i][j]=u[i][j]+dt*(D*(u[i-1][j]+u[i+1][j]+u[i][99]+u[i][j+1]-4*u[i][j]));
-     }
-
-     //j=99
-     j=99;
-     for(i=1; i<99; i++){
-       utemp[i][j]=u[i][j]+dt*(D*(u[i-1][j]+u[i+1][j]+u[i][j-1]+u[i][0]-4*u[i][j]));
-     }
-
-     //その他
-     for(i=1; i<99; i++){
-       for(j=1; j<99; j++){
-       utemp[i][j]=u[i][j]+dt*(D*(u[i-1][j]+u[i+1][j]+u[i][j-1]+u[i][j+1]-4*u[i][j]));
-     }
+  //i=0
+  i=0;
+  for(j=1;j<99;j++){
+    utemp[i][j]=u[i][j]+dt*(D*(u[99][j]+u[i+1][j]+u[i][j-1]+u[i][j+1]-4*u[i][j])+u[i][j]*u[i][j]/v-u[i][j]);
   }
-
+  //i=99
+  i=99;
+  for(j=1;j<99;j++){
+    utemp[i][j]=u[i][j]+dt*(D*(u[i-1][j]+u[0][j]+u[i][j-1]+u[i][j+1]-4*u[i][j])+u[i][j]*u[i][j]/v-u[i][j]);
+  }
+  //j=0
+  j=0;
+  for(i=1;i<99;i++){
+    utemp[i][j]=u[i][j]+dt*(D*(u[i-1][j]+u[i+1][j]+u[i][99]+u[i][j+1]-4*u[i][j])+u[i][j]*u[i][j]/v-u[i][j]);
+  }
+  //j=99
+  j=99;
+  for(i=1;i<99;i++){
+    utemp[i][j]=u[i][j]+dt*(D*(u[i-1][j]+u[i+1][j]+u[i][j-1]+u[i][0]-4*u[i][j])+u[i][j]*u[i][j]/v-u[i][j]);
+  }
+  //その他
+  for(i=1;i<99;i++){
+    for(j=1;j<99;j++){
+      utemp[i][j]=u[i][j]+dt*(D*(u[i-1][j]+u[i+1][j]+u[i][j-1]+u[i][j+1]-4*u[i][j])+u[i][j]*u[i][j]/v-u[i][j]);
+    }
+  }
   //更新
-  for(i=0; i<100; i++){
-    for(j=0; j<100; j++){
+  for(i=0;i<100;i++){
+    for(j=0;j<100;j++){
       u[i][j]=utemp[i][j];
     }
-  }
-
-  //出力
-  if(t%500==0){
-    for(i=0; i<100; i++){
-      for(j=0; j<100; j++){
-        fprintf(fp, "%f", u[i][j]);
-        if(j!=99){
-          fprintf(fp, " ,");
-        }
+}
+//出力
+if(t%500==0){
+  for(i=0;i<100;i++){
+    for(j=0;j<100;j++){
+      fprintf(fp,"%f",u[i][j]);
+      if(j!=99){
+        fprintf(fp,",");
       }
-      fprintf(fp, "\n");
     }
+    fprintf(fp,"\n");
   }
+}
+
 }
 
 fclose(fp);
---
+++ 11/11-3.c
@@ -0,0 +1,116 @@
+#include <stdio.h>
+
+int main(){
+  int i,j;
+  int t;
+  double dt=0.01;
+  double u[100][100];
+  double utemp[100][100];
+
+  double D=0.4, v=1.0;
+
+  FILE *fp;
+
+  fp=fopen("11-3.csv","w");
+
+  //初期値
+  for(i=0; i<100; i++){
+    for(j=0; j<100; j++){
+      u[i][j]=0;
+    }
+  }
+
+  u[49][49]=1;
+  u[49][50]=1;
+  u[50][49]=1;
+  u[50][50]=1;
+
+  //初期値出力
+  for(i=0; i<100; i++){
+    for(j=0; j<100; j++){
+      fprintf(fp, "%f", u[i][j]);
+      if(j!=99){
+        fprintf(fp, " ,");
+      }
+    }
+    fprintf(fp, "\n");
+  }
+
+  for(t=1; t<5000; t++){
+
+    //境界条件
+     //i=0, j=0
+     i=0;
+     j=0;
+     utemp[i][j]=u[i][j]+dt*(D*(u[99][j]+u[i+1][j]+u[i][99]+u[i][j+1]-4*u[i][j]))-dt*u[i][j]+v*v;
+
+     //i=0, j=99
+     i=0;
+     j=99;
+     utemp[i][j]=u[i][j]+dt*(D*(u[99][j]+u[i+1][j]+u[i][j-1]+u[i][0]-4*u[i][j]))-dt*u[i][j]+v*v;
+
+     //i=99, j=0
+     i=99;
+     j=0;
+     utemp[i][j]=u[i][j]+dt*(D*(u[i-1][j]+u[0][j]+u[i][99]+u[i][j+1]-4*u[i][j]))-dt*u[i][j]+v*v;
+
+     //i=99, j=99
+     i=99;
+     j=99;
+     utemp[i][j]=u[i][j]+dt*(D*(u[i-1][j]+u[0][j]+u[i][j-1]+u[i][0]-4*u[i][j]))-dt*u[i][j]+v*v;
+
+     //i=0
+     i=0;
+     for(j=1; j<99; j++){
+       utemp[i][j]=u[i][j]+dt*(D*(u[99][j]+u[i+1][j]+u[i][j-1]+u[i][j+1]-4*u[i][j]))-dt*u[i][j]+v*v;
+     }
+
+     //i=99
+     i=99;
+     for(j=1; j<99; j++){
+       utemp[i][j]=u[i][j]+dt*(D*(u[i-1][j]+u[0][j]+u[i][j-1]+u[i][j+1]-4*u[i][j]))-dt*u[i][j]+v*v;
+     }
+
+     //j=0
+     j=0;
+     for(i=1; i<99; i++){
+       utemp[i][j]=u[i][j]+dt*(D*(u[i-1][j]+u[i+1][j]+u[i][99]+u[i][j+1]-4*u[i][j]))-dt*u[i][j]+v*v;
+     }
+
+     //j=99
+     j=99;
+     for(i=1; i<99; i++){
+       utemp[i][j]=u[i][j]+dt*(D*(u[i-1][j]+u[i+1][j]+u[i][j-1]+u[i][0]-4*u[i][j]))-dt*u[i][j]+v*v;
+     }
+
+     //その他
+     for(i=1; i<99; i++){
+       for(j=1; j<99; j++){
+       utemp[i][j]=u[i][j]+dt*(D*(u[i-1][j]+u[i+1][j]+u[i][j-1]+u[i][j+1]-4*u[i][j]))-dt*u[i][j]+v*v;
+     }
+  }
+
+  //更新
+  for(i=0; i<100; i++){
+    for(j=0; j<100; j++){
+      u[i][j]=utemp[i][j];
+    }
+  }
+
+  //出力
+  if(t%500==0){
+    for(i=0; i<100; i++){
+      for(j=0; j<100; j++){
+        fprintf(fp, "%f", u[i][j]);
+        if(j!=99){
+          fprintf(fp, " ,");
+        }
+      }
+      fprintf(fp, "\n");
+    }
+  }
+}
+
+fclose(fp);
+return 0;
+}
